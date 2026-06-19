a715cf7568b2b6b2e34c121a38d4e2c51bd5336a
Aditya Singh <singh.aditya.rajput10@gmail.com>
2026-06-19 10:35:20 +0530

    root cause

--- server.js
+++ server.js
@@ -28,6 +28,9 @@ app.use("/api/contact", contactRoute);
 app.use("/api/admin", adminRoutes);
 app.use(express.static("public"));
 
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "index.html"));
+});
 
 app.get("/profile", authMiddleware.authM, (req, res) => {
     res.json({
