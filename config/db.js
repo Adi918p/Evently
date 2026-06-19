17546e3c5d2644254e032cd49da05474d12b5cad
Aditya Singh <singh.aditya.rajput10@gmail.com>
2026-06-19 10:25:00 +0530

    db

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 async function connectDB() {
     try {
         await mongoose.connect(
-            process.env.MONGO_URI || "mongodb://127.0.0.1:27017/evently"
+            process.env.MONGO_URI
         );
 
         console.log("MongoDB Connected");
